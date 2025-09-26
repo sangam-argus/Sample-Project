2826496eb6cbd5508bc5950fa73cbd836f3c9a86
Sangam Arora <sangam.arora@argusoft.com>
2025-09-26 13:22:30 +0530

    more styling added

--- Receipe Generator/src/pages/Favourites.tsx
+++ Receipe Generator/src/pages/Favourites.tsx
@@ -2,12 +2,20 @@ import Cards from "../components/Cards/Cards";
 import type { Receipe } from "../util/Constant";
 import "./Home.css";
 import useGlobalContext from "../hooks/useGlobalContext";
+import { centerText } from "../util/style";
 
 function Favourites() {
   const { favourite, receipe } = useGlobalContext();
 
   if (favourite.length === 0 || receipe.length === 0) {
-    return <p>No Favourites</p>;
+    return (
+      <div
+        style={centerText}
+      >
+        <h2>No Favourites, Please mark some items as favourite</h2>
+
+      </div>
+    );
   }
   return (
     <div className="cardsContainer">
--- Receipe Generator/src/pages/Home.tsx
+++ Receipe Generator/src/pages/Home.tsx
@@ -2,12 +2,17 @@ import Cards from "../components/Cards/Cards";
 import type { Receipe } from "../util/Constant";
 import "./Home.css";
 import useGlobalContext from "../hooks/useGlobalContext";
+import { centerText } from "../util/style";
 
 function Home() {
-  const { receipe,loading }=useGlobalContext()
+  const { receipe, loading } = useGlobalContext();
 
-  if(loading){
-    return <h2>Loading Please wait ....</h2>
+  if (loading) {
+    return (
+      <div style={centerText}>
+        <h2>Loading Please wait ....</h2>
+      </div>
+    );
   }
   return (
     <>
@@ -18,7 +23,9 @@ function Home() {
           ))}
         </div>
       ) : (
-        <h2>Nothing to show Please search something....</h2>
+        <div style={centerText}>
+          <h2>Nothing to show Please search something....</h2>
+        </div>
       )}
     </>
   );
